de194c16437480e4444a780b96a7b0e8f40ff6b4
Leelakrishna K <kleelakrishna753@gmail.com>
2024-11-27 19:49:38 +0530

    Signup

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
 export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div>
+            <p>
+                <input type="text" placeholder="Enter Name"></input>
+            </p>        
+            <p>
+                <input type="text" placeholder="Enter Phone Number"></input>
+            </p>
+            <p>
+                <input type="text" placeholder="Enter Email Address"></input>
+            </p>
+            <p>
+                <input type="text" placeholder="Enter OTP"></input>
+            </p>
+            <p>
+                <input type="password" placeholder="Enter Password"></input>
+            </p>
+            <p>
+                <input type="password" placeholder="Retype Password"></input>
+            </p>
+            <button>Signup</button>
+            <hr></hr>
+        </div>
+      )
 }
